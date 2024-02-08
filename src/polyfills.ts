63ca10f1a9e29022a4fc31fce561160bbf51fe5c
Ramon Montilla <ramontilla@gmail.com>
2024-02-08 19:08:19 -0400

    version inicial

---
+++ src/polyfills.ts
@@ -0,0 +1,8 @@
+/* eslint-disable
+@typescript-eslint/no-explicit-any
+*/
+import { Buffer } from "buffer";
+
+(window as any).global = window;
+(window as any).global.Buffer = Buffer;
+(window as any).process = { env: { } };
