469106dba2ed37b549f245e9119e697c641dbae0
Ramon Montilla <ramontilla@gmail.com>
2024-02-13 23:22:43 -0400

    feat: mostrar balance de silly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { ShyftApiService } from './services/shyft-api.service';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { computedAsync } from 'ngxtension/computed-async';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    RouterModule,
+    RouterOutlet,
+    RouterLink,
+    DecimalPipe,
+    HdWalletMultiButtonComponent,
+  ],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+    <header class="px-16 pt-20 pb-8 relative">
+      <h1 class="text-center text-5xl mb-4"> Heavy Wallet</h1>
+
+      <div class="flex justify-center mb-4">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
+
+      @if (account()) {
+        <div class="absolute top-4 left-4 flex justify-center items-center gap-2">
+          <img [src]="account()?.info?.image" class="w-8 h-8" />
+          <p class="text-2xl font-bold">
+            {{ account()?.balance | number }}
+          </p>
+        </div>
+      }
+    </header>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `,
 })
 export class AppComponent {
-  title = 'app';
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    { requireSync: true },
+  );
 }
